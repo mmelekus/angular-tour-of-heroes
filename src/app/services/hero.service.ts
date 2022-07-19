e7fc98dab9caf6bd11eb958575f81b86962a6d22
MartinMZen <mmelekus@msn.com>
2022-07-19 11:46:27 -0500

    Get one hero and upate hero on service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
@@ -11,23 +12,63 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   private heroesUrl: string = 'api/heroes'; // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
     /** GET heroes from the server */
-    return this.http.get<Hero[]>(this.heroesUrl);
+    getHeroes(): Observable<Hero[]> {
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
-  getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified 'id' always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
-    this.log(`fetched hero id=${id}`);
-    return of(hero);
+   /** GET hero by id.  Will 404 if id not found */
+   getHero(id: number): Observable<Hero> {
+   const url = `${this.heroesUrl}/${id}`;
+   return this.http
+    .get<Hero>(url)
+    .pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http
+      .put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * 
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returing an empty result.
+      return of(result as T);
+    }
   }
 }
